9a7e6f7c2fe158af78a5c63bd52f6d73211b1789
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2023-07-14 19:03:45 +0530

    new commit

---
+++ Day2/FindDuplicateinArray.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+int findDuplicate(vector<int> &arr,int n){
+    sort(arr.begin(),arr.end());
+    for(int i=1;i<arr.size();i++){
+        if(arr[i]==arr[i-1]){
+            return arr[i];
+        }
+    }
+    return -1;
+}
+
