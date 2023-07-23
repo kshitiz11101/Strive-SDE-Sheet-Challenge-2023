8352bc6d1f913f56106d9ce6afb1aecbbbf44ac4
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2023-07-23 13:28:34 +0530

    new commit

---
+++ Day13/NextPermutation.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+
+#include <vector>
+
+vector<int> nextPermutation(vector<int> &A, int n)
+
+{
+
+  //  Write your code here.
+
+  next_permutation(A.begin(), A.end());
+
+  return A;
+}
