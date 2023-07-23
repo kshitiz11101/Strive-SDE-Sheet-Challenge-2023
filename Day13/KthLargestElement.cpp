f805b6f74b377814a6bdb9c26e288b15eb2d295e
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2023-07-23 13:58:51 +0530

    new commit

---
+++ Day13/KthLargestElement.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h> 
+/************************************************************
+    Following is the Binary Search Tree node structure
+    
+    template <typename T>
+    class TreeNode {
+        public :
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~TreeNode() {
+            if(left)
+                delete left;
+            if(right)
+                delete right;
+        }
+    };
+
+************************************************************/
+void inorder(TreeNode<int>* root,vector<int> &ans){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left,ans);
+    ans.push_back(root->data);
+    inorder(root->right,ans);
+}
+int KthLargestNumber(TreeNode<int>* root, int k) 
+{
+    // Write your code here.
+    vector<int> ans;
+    inorder(root,ans);
+    int n=ans.size();
+    if(k>n){
+        return -1;
+    }
+
+    return ans[n-k];
+}
