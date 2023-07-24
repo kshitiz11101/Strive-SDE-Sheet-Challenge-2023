c9eb8f1b51f02412debfe79af0e0fad8a5363c51
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2023-07-24 19:24:26 +0530

    new commit

---
+++ Day14/LongestSubarrayZeroSum.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+int LongestSubsetWithZeroSum(vector<int> arr)
+{
+
+  int ans = 0;
+
+  int n = arr.size();
+
+  int sum = 0;
+
+  unordered_map<int, int> mp;
+
+  for (int i = 0; i < n; i++)
+  {
+
+    sum += arr[i];
+
+    if (sum == 0)
+    {
+
+      ans = i + 1;
+    }
+    else
+    {
+
+      if (mp.find(sum) != mp.end())
+      {
+
+        ans = max(ans, i - mp[sum]);
+      }
+      else
+      {
+
+        mp[sum] = i;
+      }
+    }
+  }
+
+  return ans;
+}
