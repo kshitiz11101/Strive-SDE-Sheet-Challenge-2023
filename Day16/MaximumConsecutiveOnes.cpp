220632aaf171277e105c82674c9e8fa8b150175d
Kshitiz Sharma <110187324+kshitiz11101@users.noreply.github.com>
2023-07-26 19:18:45 +0530

    new commit

---
+++ Day16/MaximumConsecutiveOnes.cpp
@@ -0,0 +1,22 @@
+int longestSubSeg(vector<int> &arr , int n, int k){
+    // Write your code here.
+    int i=0,j=0;
+
+    for( i=0;i<n;i++){
+
+        if(arr[i]==0){
+
+            k--;
+
+        }
+
+        if(k<0 && arr[j++]==0){
+
+            k++;
+
+        }
+
+    }
+
+    return i-j;
+}
